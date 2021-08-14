a065ab339a38a438db6bb093ff03c78a4f192f94
Arasti <NateArasti@users.noreply.github.com>
2021-08-14 15:23:56 +0500

    BuildUp for all desktop platforms

--- Assets/Player/InputSystem.cs
+++ Assets/Player/InputSystem.cs
@@ -15,18 +15,21 @@ public class InputSystem : MonoBehaviour
 
     private bool isPaused;
 
-    //private bool hadSecondTouch;
-    //private int fingerId = -1;
-
     private void Update()
     {
-        var explosionActionState = CheckExplosionAction();
-        if (explosionActionState is true)
-            onExplosionStartPress.Invoke();
-        else if (explosionActionState is false) 
-            onExplosionEndPress.Invoke();
-        if (CheckZoomAction()) onZoom.Invoke();
         if (CheckPauseAction()) onPause.Invoke(isPaused);
+        if (isPaused) return;
+        if (Input.GetMouseButtonDown(0)) onExplosionStartPress.Invoke();
+        else if (Input.GetMouseButtonUp(0) && Time.time - lastExplode > CoolDown)
+        {
+            onExplosionEndPress.Invoke();
+            lastExplode = Time.time;
+        }
+        else if (Input.GetMouseButtonDown(1) && Time.time - lastZoom > CoolDown)
+        {
+            onZoom.Invoke();
+            lastZoom = Time.time;
+        }
     }
 
     public void UnPause()
@@ -36,62 +39,10 @@ public class InputSystem : MonoBehaviour
         onPause.Invoke(false);
     }
 
-    private bool? CheckExplosionAction()
-    {
-        if (Time.time - lastExplode < CoolDown || isPaused) return null;
-        if (Input.GetMouseButtonDown(0)) return true;
-        if (Input.GetMouseButtonUp(0))
-        {
-            lastExplode = Time.time;
-            return false;
-        }
-        return null;
-    }
-
-    private bool CheckZoomAction()
-    {
-        var result = Time.time - lastZoom > CoolDown && Input.GetMouseButtonUp(1) && !isPaused;
-        if (result) lastZoom = Time.time;
-        return result;
-    }
-
     private bool CheckPauseAction()
     {
         if (!Input.GetKeyDown(KeyCode.Escape)) return false;
         isPaused = !isPaused;
         return true;
     }
-
-
-    //Android Controls
-    //private bool ExplosionInput()
-    //{
-    //    if (Input.touchCount == 0) return false;
-    //    if (fingerId == -1)
-    //        fingerId = Input.GetTouch(0).fingerId;
-    //    if (Input.GetTouch(fingerId).phase != TouchPhase.Ended)
-    //    {
-    //        if (!hadSecondTouch) hadSecondTouch = Input.touchCount == 2;
-    //        return false;
-    //    }
-
-    //    fingerId = -1;
-    //    if (hadSecondTouch)
-    //    {
-    //        hadSecondTouch = false;
-    //        return false;
-    //    }
-
-    //    var result = Time.time - lastExplode > CoolDown;
-    //    if (result) lastExplode = Time.time;
-    //    return result;
-
-    //}
-
-    //private bool ZoomInput()
-    //{
-    //    var result = Input.touchCount == 2 && Time.time - lastZoom > CoolDown;
-    //    if (result) lastZoom = Time.time;
-    //    return result;
-    //}
 }
